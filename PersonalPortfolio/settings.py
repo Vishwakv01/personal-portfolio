b791fe33cee4f21d084f547178c17f196cb8826c
Vishwakv01 <86126189+Vishwakv01@users.noreply.github.com>
2022-01-21 00:46:28 +0530

    Update setting.py

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g__4fv-*v9fk#5-%wu6%)7!+m-7j%qq)1%m6^b)ilxh&x4te1e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
